47143741eed41aa4f5744e413f91073368e594c9
andd1791 <andd1791@gmail.com>
2024-05-10 19:57:30 +0700

    commit code

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: "Javascript Tutorial",
+        productNumber: "85745",
+        paymentStatus: "Due",
+        status: "Pending"
+    },
+    {
+        productName: "CSS Full Course",
+        productNumber: "19694",
+        paymentStatus: "Refunded",
+        status: "Declined"
+    },
+    {
+        productName: "Javascript Tutorial",
+        productNumber: "85745",
+        paymentStatus: "Due",
+        status: "Pending"
+    },
+    {
+        productName: "Flex-Box Tutorial",
+        productNumber: "38485",
+        paymentStatus: "Paid",
+        status: "Active"
+    },
+    {
+        productName: "Angular Tutorial",
+        productNumber: "39299",
+        paymentStatus: "Due",
+        status: "Pending"
+    },
+    {
+        productName: "Java OOP Tutorial",
+        productNumber: "56756",
+        paymentStatus: "Paid",
+        status: "Active"
+    },
+]
